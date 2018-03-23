3aad15029a490460268627c877352ce55fef7121
Julius Flimmel <flimmelj@gmail.com>
2018-03-23 14:59:15 +0100

    Added test operator

---
+++ main.py
@@ -0,0 +1,40 @@
+import bpy
+
+bl_info = {
+    'name': 'Music to 3D',
+    'category': 'Mesh'
+}
+
+
+class MusicTo3D(bpy.types.Operator):
+    """
+    Add docs
+    """
+
+    bl_idname = 'mesh.music_to_3d' # TODO: constant...
+    bl_label = 'Music To 3D'
+    bl_options = {'REGISTER', 'UNDO'}
+
+    song_path = bpy.props.StringProperty(name="Song path", description="Path to the song specifying the terrain")
+
+    def execute(self, context):
+        print("The M3D operator was executed")
+
+
+def add_terrain_mesh(self, context):
+    layout = self.layout
+    layout.separator()
+    layout.operator("mesh.music_to_3d", text="Sound Terrain", icon="MOD_SUBSURF")
+
+
+def register():
+    bpy.utils.register_class(MusicTo3D)
+    bpy.types.INFO_MT_mesh_add.append(add_terrain_mesh)
+
+
+def unregister():
+    bpy.utils.unregister_class(MusicTo3D)
+
+
+if __name__ == '__main__':
+    register()
